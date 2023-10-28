92c72493471d173fd8c1e0b6ccc1ae6efa60df69
Wincent Balin <wincent.balin@gmail.com>
2023-10-29 01:05:26 +0200

    Added processing of multiple input files

--- imagescaler.cpp
+++ imagescaler.cpp
@@ -11,19 +11,30 @@ int main(int argc, char **argv)
             ("help,h", "Print help message")
             ("width", po::value<int>(), "Width to scale the image to")
             ("height", po::value<int>(),"Height to scale the image to")
+            ("input-file", po::value< vector<string> >(), "Image file or maybe (if last) output directory")
     ;
 
     po::positional_options_description p;
     p.add("input-file", -1);
 
     po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::store(po::command_line_parser(argc, argv).
+              options(desc).positional(p).run(), vm);
     po::notify(vm);
 
     if (vm.count("help")) {
         cout << "Usage: imagescaler option(s) file(s)" << endl;
         cout << desc;
         return 0;
+    }
+
+    if (!vm.count("input-file")) {
+        cerr << "Specify at least one file, the last one might be output directory" << endl;
+        return 1;
+    }
+
+    for (auto &fn: vm["input-file"].as< vector<string> >()) {
+        cout << fn << endl;
     }
 
     return 0;
